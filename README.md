b49c0ff5ab5bde91326a4c63b4b0a6dca416f4f1
Stiven <stivendor101@gmail.com>
2024-02-06 07:33:05 -0500

    realización de los primeros ejercicios con js

--- README.md
+++
@@ -1,2 +0,0 @@
-# JavaScript
-Seguimiento de los ejercicios propuestos en The Odin Project con JavaScript
---
+++ ejercicios-1/index.js
@@ -0,0 +1,47 @@
+/*Añade 2 números juntos (solo tipo console.log(23 + 97) en su archivo HTML)*/
+
+console.log(20 + 92);
+console.log(51 + 23);
+
+/*Agregue una secuencia de 6 números diferentes juntos.*/
+
+let numero = "1 2 3 4 5 6";
+
+console.log(numero);
+
+/*Imprima la solución a la siguiente ecuación: (4 + 6 + 9) / 77*/
+
+let A = 4
+    B = 6
+    C = 9
+    suma = A + B + C
+
+let dividendo = "77"
+
+console.log(suma / dividendo);
+
+/*VARIABLES*/
+ 
+/*En la consola console.log(a) debe imprimir 10*/
+
+let a = 10
+
+console.log(a);
+
+/*Pruebe lo siguiente en la consola: 9 * a*/
+
+console.log(9 * a);
+
+/*y esto: let b = 7 * a (devoluciones undefined *) y luego console.log(b)*/
+
+let b = 7 * a
+
+console.log(b);
+
+/*SECUENCIA*/
+
+const MAX = 57
+const actual = MAX - 14
+const percentage = actual / MAX
+
+console.log(percentage);
